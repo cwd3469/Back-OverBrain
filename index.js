c2d8c7e99179f3e9243c3e789ebf5ffb558bee95
jooyoung-choi-bepluslab <cwd3469@beplushealthcare.com>
2024-08-23 16:00:04 +0900

    main node setUp

---
+++ index.js
@@ -0,0 +1,17 @@
+import OpenAI from 'openai';
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const completion = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: [
+        { role: 'system', content: 'You are a helpful assistant' },
+        {
+            role: 'user',
+            content: 'Write a haiku about recursion in programming',
+        },
+    ],
+});
+
+console.log(completion.choices[0].message);
